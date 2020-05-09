eab49083b8375b90a7301717968b86204db57c94
JBladejs <JBladejs@gmail.com>
2020-05-09 23:40:35 +0200

    added class representing board field

---
+++ core/src/com/bladejs/chess/entities/BoardField.kt
@@ -0,0 +1,4 @@
+package com.bladejs.chess.entities
+
+class BoardField {
+}
--- core/src/com/bladejs/chess/entities/GameBoard.kt
+++ core/src/com/bladejs/chess/entities/GameBoard.kt
@@ -1,4 +1,7 @@
 package com.bladejs.chess.entities
 
+import com.badlogic.gdx.utils.Array
+
 class GameBoard {
+    private val board = Array<Array<BoardField>>()
 }
