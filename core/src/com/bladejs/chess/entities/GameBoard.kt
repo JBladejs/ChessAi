b715475e60a50f8c76bc0f4b5a6ac29c0b2edd13
JBladejs <JBladejs@gmail.com>
2020-05-10 02:39:51 +0200

    implemented drawing of the chess board

--- core/src/com/bladejs/chess/entities/GameBoard.kt
+++ core/src/com/bladejs/chess/entities/GameBoard.kt
@@ -16,10 +16,18 @@ class GameBoard {
             color.set(Color.BROWN)
             val cellSize = Gdx.graphics.height / 9f
             val halfCellSize = cellSize / 2f
+            val doubleCellSize = cellSize * 2f
             rect(0f, 0f, halfCellSize, Gdx.graphics.height.toFloat())
             rect(0f, 0f, Gdx.graphics.width.toFloat(), halfCellSize)
             rect(0f, Gdx.graphics.height.toFloat() - halfCellSize, Gdx.graphics.width.toFloat(), halfCellSize)
             rect(Gdx.graphics.width.toFloat() - halfCellSize, 0f, halfCellSize, Gdx.graphics.height.toFloat())
+            color.set(Color.WHITE)
+            for (i in 0..7) {
+                for (j in 0..3) {
+                    val start = if (i % 2 == 0) cellSize + halfCellSize else halfCellSize
+                    rect(start + (j * doubleCellSize), halfCellSize + (i * cellSize), cellSize, cellSize)
+                }
+            }
             end()
         }
     }
