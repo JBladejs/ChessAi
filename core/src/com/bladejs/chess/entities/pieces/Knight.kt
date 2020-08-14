cb8a78b2bca636f4e8f6bfe25f1ce6290d07c99f
Bladejs <JBladejs@gmail.com>
2020-08-14 14:16:50 +0200

    Implemented Knight's movement rules

--- core/src/com/bladejs/chess/entities/pieces/Knight.kt
+++ core/src/com/bladejs/chess/entities/pieces/Knight.kt
@@ -6,6 +6,15 @@ import com.bladejs.chess.misc.Position
 
 class Knight(x: Int, y: Int, color: Color) : Piece(Texture("knight.png"), Texture("knightB.png"), x, y, color) {
     override fun getAvailableMoves(): Array<Position> {
-        TODO("Not yet implemented")
+        val positions = Array<Position>()
+        positions.add(checkForMove(x - 1, y + 2, true))
+        positions.add(checkForMove(x + 1, y + 2, true))
+        positions.add(checkForMove(x - 1, y - 2, true))
+        positions.add(checkForMove(x + 1, y - 2, true))
+        positions.add(checkForMove(x + 2, y - 1, true))
+        positions.add(checkForMove(x + 2, y + 1, true))
+        positions.add(checkForMove(x - 2, y - 1, true))
+        positions.add(checkForMove(x - 2, y + 1, true))
+        return positions
     }
 }
