d44f5076d6912602d3fd5a1dba67e3f75293c428
Bladejs <JBladejs@gmail.com>
2020-08-14 14:24:45 +0200

    Implemented King's movement pattern

--- core/src/com/bladejs/chess/entities/pieces/King.kt
+++ core/src/com/bladejs/chess/entities/pieces/King.kt
@@ -6,6 +6,15 @@ import com.bladejs.chess.misc.Position
 
 class King(x: Int, y: Int, color: Color) : Piece(Texture("king.png"), Texture("kingB.png"), x, y, color) {
     override fun getAvailableMoves(): Array<Position> {
-        TODO("Not yet implemented")
+        val positions = Array<Position>()
+        positions.add(checkForMove(x, y + 1, true))
+        positions.add(checkForMove(x + 1, y + 1, true))
+        positions.add(checkForMove(x + 1, y, true))
+        positions.add(checkForMove(x + 1, y - 1, true))
+        positions.add(checkForMove(x, y - 1, true))
+        positions.add(checkForMove(x - 1, y - 1, true))
+        positions.add(checkForMove(x - 1, y, true))
+        positions.add(checkForMove(x - 1, y + 1, true))
+        return positions
     }
 }
